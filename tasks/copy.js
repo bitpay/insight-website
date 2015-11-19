abeddb8929e73761503a90c55df30c074dbc14eb
Jason Dreyzehner <jason@bitpay.com>
2015-11-19 15:47:54 -0500

    copy legacy in build

--- tasks/copy.js
+++ tasks/copy.js
@@ -4,10 +4,16 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 // Copy files at root level of 'src' to dist
-gulp.task('copy', function() {
+gulp.task('copy', ['legacy'], function() {
   return gulp.src(['src/*', '!src/_*', '!src/*.jade'], {
       dot: true
     })
     .pipe($.cached('copy'))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('legacy', function() {
+  return gulp.src(['src/legacy/**/*'])
+    .pipe($.debug())
+    .pipe(gulp.dest('dist/legacy'));
 });
